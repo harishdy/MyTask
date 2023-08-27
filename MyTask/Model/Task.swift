c73b6d21d40b62f46e4a7a7bbb4686b1856cb070
Harish yadav <harishdy@gmail.com>
2023-08-27 12:22:08 +0530

    On Day 3 show details view on list click is completed

--- MyTask/Model/Task.swift
+++ MyTask/Model/Task.swift
@@ -10,14 +10,14 @@ struct Task{
     let id:Int
     var name:String
     var description:String
-    var isActive:Bool
+    var isCompleted:Bool
     var finishDate:Date
     
     static func createMockTasks()->[Task]{
         return[
-            Task(id: 1, name: "Go to gym", description: "back workout", isActive: true , finishDate: Date()),
-            Task(id: 2, name: "Car wash", description: "Down cwash center", isActive: true , finishDate: Date()),
-            Task(id: 3, name: "Office work", description: "Finsh picker module", isActive: false , finishDate: Date())
+            Task(id: 1, name: "Go to gym", description: "back workout", isCompleted: false , finishDate: Date()),
+            Task(id: 2, name: "Car wash", description: "Down cwash center", isCompleted: false , finishDate: Date()),
+            Task(id: 3, name: "Office work", description: "Finsh picker module", isCompleted: true , finishDate: Date())
         ]
     }
 }
